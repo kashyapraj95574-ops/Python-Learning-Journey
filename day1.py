d80bb95d5808b1807911395677bffc162d1c6aae
kashyapraj95574-ops <kashyapraj95574@gmail.com>
2026-07-06 23:17:36 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,32 @@
+name="rajan"
+age="22"
+address="nanauta"
+email="kashyapraj95574@gmail.com"
+print(name)
+print(age)
+print(address)
+print(email)
+
+
+a=2
+b=4
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+
+
+l=2
+b=4
+area =(l*b)
+print(area)
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit is:", fahrenheit)
+
+
+
+name=input("enter name")
+print("hello",name)
